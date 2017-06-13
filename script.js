ba09430891fb2f0cef746044ee69ffdf45b2f679
linseeh <shiruba.rein15@gmail.com>
2017-06-13 12:17:50 +0800

    added disappearing div

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function(){
-	document.getElementById("allyn-pic").onclick = a-showDiv;
-	document.getElementById("lincy-pic").onclick = l-showDiv;
+	document.getElementById("allyn-pic").onclick = a_showDiv;
+	document.getElementById("lincy-pic").onclick = l_showDiv;
+
+	console.log("script");
 }
 
-function a-showDiv() {
+function a_showDiv() {
 	var div = document.getElementById("allyn-div");
 	if (div.style.display === 'none') {
 		div.style.display = 'block';
@@ -13,7 +15,7 @@ function a-showDiv() {
 	}
 }
 
-function l-showDiv() {
+function l_showDiv() {
 	var div = document.getElementById("lincy-div");
 	if (div.style.display === 'none') {
 		div.style.display = 'block';
